8ee5fb1a62b67df3be6cb8105e5f3a5f25af08d2
Joel Nicolas Sartori <joelnicolass@gmail.com>
2025-05-29 13:16:28 -0300

    refactor: mejorar la estructura y estilos de los componentes GameScreen y HomeScreen

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -25,104 +25,106 @@ const GameScreen = () => {
   }, [actionCards]);
 
   return (
-    <div className="flex flex-col items-center gap-4 h-full w-full">
-      <div
-        style={{
-          position: "absolute",
-          width: "100svw",
-          height: "100svh",
-          display: "flex",
-          overflow: "hidden",
-        }}
-      >
-        <Spotlight />
+    <>
+      <div className="w-screen h-screen fixed top-0 left-0 z-50 pointer-events-none">
+        <Spotlight
+          width={window.innerWidth}
+          height={window.innerHeight}
+          gradientFirst="radial-gradient(
+                rgba(255, 255, 255, 0.1) 0%,
+                rgba(141, 12, 233, 0.04) 50%,
+                rgba(210, 100, 45, 0.0) 80%
+              )"
+        />
       </div>
-      <section
-        style={{
-          width: "100svw",
-          height: "100svh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "20px",
-          maxWidth: "1024px",
-        }}
-      >
-        <Card
+      <div className="flex flex-col items-center gap-4 h-full w-full">
+        <section
           style={{
-            width: "100%",
+            width: "100svw",
+            height: "100svh",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            backgroundColor: "rgba(255, 255, 255, 0.1)",
             padding: "20px",
-            backdropFilter: "blur(20px)",
-            WebkitBackdropFilter: "blur(20px)",
+            maxWidth: "1024px",
           }}
         >
-          <div
-            className="flex items-center gap-6 flex-wrap justify-center"
-            style={{ textShadow: "0 0 10px rgba(0, 0, 0, 0.5)" }}
-          >
-            <img
-              src={leaderSelected.image}
-              alt={leaderSelected.name}
-              draggable={false}
-              style={{
-                maxWidth: "200px",
-                borderRadius: "10px",
-                width: "100%",
-              }}
-            />
-
-            <Swiper
-              key={swiperKey}
-              effect={"cards"}
-              grabCursor={true}
-              modules={[EffectCards]}
-              className="w-[50%] h-[50%] max-w-[350px] max-h-[500px]"
-              loop
-              direction="horizontal"
-              allowSlideNext={false}
-              onRealIndexChange={(swiper) => {
-                if (swiper.realIndex === 0) {
-                  startTransition(() => {
-                    setCards(shuffleArray(cards));
-                    setSwiperKey((prev) => prev + 1);
-                  });
-                }
-              }}
-            >
-              {cards.map((card) => (
-                <SwiperSlide
-                  key={card.name}
-                  className="bg-transparent border-2 rounded-[10px] border-[#0000004b] shadow-[0_0_10px_#0000005f]"
-                >
-                  <img
-                    src={card.image}
-                    alt={card.name}
-                    draggable={false}
-                    className="w-full h-full object-fill rounded-lg pointer-none"
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-
-          <Separator />
-          <Button
-            onClick={() => {
-              startTransition(() => {
-                resetAll();
-                navigation.push("/");
-              });
+          <Card
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+              padding: "20px",
+              backdropFilter: "blur(20px)",
+              WebkitBackdropFilter: "blur(20px)",
             }}
           >
-            Volver
-          </Button>
-        </Card>
-      </section>
-    </div>
+            <div
+              className="flex items-center gap-6 flex-wrap justify-center"
+              style={{ textShadow: "0 0 10px rgba(0, 0, 0, 0.5)" }}
+            >
+              <img
+                src={leaderSelected.image}
+                alt={leaderSelected.name}
+                draggable={false}
+                style={{
+                  maxWidth: "200px",
+                  borderRadius: "10px",
+                  width: "100%",
+                }}
+              />
+
+              <Swiper
+                key={swiperKey}
+                effect={"cards"}
+                grabCursor={true}
+                modules={[EffectCards]}
+                className="w-[50%] h-[50%] max-w-[350px] max-h-[500px]"
+                loop
+                direction="horizontal"
+                allowSlideNext={false}
+                onRealIndexChange={(swiper) => {
+                  if (swiper.realIndex === 0) {
+                    startTransition(() => {
+                      setCards(shuffleArray(cards));
+                      setSwiperKey((prev) => prev + 1);
+                    });
+                  }
+                }}
+              >
+                {cards.map((card) => (
+                  <SwiperSlide
+                    key={card.name}
+                    className="bg-transparent border-2 rounded-[10px] border-[#0000004b] shadow-[0_0_10px_#0000005f]"
+                  >
+                    <img
+                      src={card.image}
+                      alt={card.name}
+                      draggable={false}
+                      className="w-full h-full object-fill rounded-lg pointer-none"
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+
+            <Separator />
+            <Button
+              onClick={() => {
+                startTransition(() => {
+                  resetAll();
+                  navigation.push("/");
+                });
+              }}
+            >
+              Volver
+            </Button>
+          </Card>
+        </section>
+      </div>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,111 +25,112 @@ const HomeScreen = () => {
   const navigation = useRouter();
 
   return (
-    <div className="flex flex-col items-center h-screen gap-4">
-      <div
-        style={{
-          position: "absolute",
-          width: "100svw",
-          height: "100svh",
-          display: "flex",
-          overflow: "hidden",
-        }}
-      >
-        <Spotlight />
+    <>
+      <div className="w-screen h-screen fixed top-0 left-0 z-50 pointer-events-none">
+        <Spotlight
+          width={window.innerWidth}
+          height={window.innerHeight}
+          gradientFirst="radial-gradient(
+            rgba(255, 255, 255, 0.1) 0%,
+            rgba(141, 12, 233, 0.04) 50%,
+            rgba(210, 100, 45, 0.0) 80%
+          )"
+        />
       </div>
-
-      <section
-        style={{
-          width: "100svw",
-          height: "100svh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          padding: "20px",
-          maxWidth: "1024px",
-        }}
-      >
-        <Card
+      <div className="flex flex-col items-center h-screen gap-4 w-screen overflow-hidden">
+        <section
           style={{
-            width: "100%",
-            maxWidth: "1024px",
+            width: "100svw",
+            height: "100svh",
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
-            backgroundColor: "rgba(255, 255, 255, 0.1)",
             padding: "20px",
-            backdropFilter: "blur(20px)",
-            WebkitBackdropFilter: "blur(20px)",
+            maxWidth: "1024px",
           }}
         >
-          <span className="text-2xl font-bold text-center">
-            Elegí a tu rival
-          </span>
-          <Separator />
-          <Swiper
-            slidesPerView={1}
-            effect={"cards"}
-            grabCursor={true}
-            modules={[EffectCards]}
-            className="w-[50%] h-[50%] max-w-[350px] max-h-[600px]"
-            loop
-            direction="horizontal"
-            allowSlideNext={false}
-            onRealIndexChange={(swiper) => {
-              swiper.allowTouchMove = false;
-              swiper.unsetGrabCursor();
-              const selectedCard = leaderCards[swiper.realIndex];
-              setLeaderSelected(selectedCard);
-            }}
-            onTouchEnd={(swiper) => {
-              swiper.allowTouchMove = true;
+          <Card
+            style={{
+              width: "100%",
+              maxWidth: "1024px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "rgba(255, 255, 255, 0.1)",
+              padding: "20px",
+              backdropFilter: "blur(20px)",
+              WebkitBackdropFilter: "blur(20px)",
             }}
           >
-            {leaderCards.map((card) => (
-              <SwiperSlide
-                key={card.name}
-                className="bg-transparent border-2 rounded-[10px] border-[#0000004b] shadow-[0_0_10px_#0000005f]"
-              >
-                <img
-                  src={card.image}
-                  alt={card.name}
-                  draggable={false}
-                  className="w-full h-full object-fill rounded-lg pointer-none"
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-          <Separator />
-          <section className="flex items-center justify-between w-full">
-            <div className="flex items-center justify-center gap-8">
-              <div className="flex flex-col items-center justify-center gap-2">
-                Incluír Agora
-                <Switch
-                  checked={includeAgora}
-                  onCheckedChange={setIncludeAgora}
-                />
-              </div>
-
-              <div className="flex flex-col items-center justify-center gap-2">
-                Incluír Pantheon
-                <Switch
-                  checked={includePantheon}
-                  onCheckedChange={setIncludePantheon}
-                />
-              </div>
-            </div>
-
-            <Button
-              onClick={() => {
-                navigation.replace("/game");
+            <span className="text-2xl font-bold text-center">
+              Elegí a tu rival
+            </span>
+            <Separator />
+            <Swiper
+              slidesPerView={1}
+              effect={"cards"}
+              grabCursor={true}
+              modules={[EffectCards]}
+              className="w-[50%] h-[50%] max-w-[350px] max-h-[600px]"
+              loop
+              direction="horizontal"
+              allowSlideNext={false}
+              onRealIndexChange={(swiper) => {
+                swiper.allowTouchMove = false;
+                swiper.unsetGrabCursor();
+                const selectedCard = leaderCards[swiper.realIndex];
+                setLeaderSelected(selectedCard);
+              }}
+              onTouchEnd={(swiper) => {
+                swiper.allowTouchMove = true;
               }}
             >
-              Continuar
-            </Button>
-          </section>
-        </Card>
-      </section>
-    </div>
+              {leaderCards.map((card) => (
+                <SwiperSlide
+                  key={card.name}
+                  className="bg-transparent border-2 rounded-[10px] border-[#0000004b] shadow-[0_0_10px_#0000005f]"
+                >
+                  <img
+                    src={card.image}
+                    alt={card.name}
+                    draggable={false}
+                    className="w-full h-full object-fill rounded-lg pointer-none"
+                  />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <Separator />
+            <section className="flex items-center justify-between w-full">
+              <div className="flex items-center justify-center gap-8">
+                <div className="flex flex-col items-center justify-center gap-2">
+                  Incluír Agora
+                  <Switch
+                    checked={includeAgora}
+                    onCheckedChange={setIncludeAgora}
+                  />
+                </div>
+
+                <div className="flex flex-col items-center justify-center gap-2">
+                  Incluír Pantheon
+                  <Switch
+                    checked={includePantheon}
+                    onCheckedChange={setIncludePantheon}
+                  />
+                </div>
+              </div>
+
+              <Button
+                onClick={() => {
+                  navigation.replace("/game");
+                }}
+              >
+                Continuar
+              </Button>
+            </section>
+          </Card>
+        </section>
+      </div>
+    </>
   );
 };
 
